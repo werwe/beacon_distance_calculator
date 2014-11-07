359181e489a72143761dcd0cd739b8c16906b11a
Geono <syaokun2744@gmail.com>
2014-11-07 17:41:06 +0900

    distancecalculator

--- distancecalculator.py
+++ distancecalculator.py
@@ -1,3 +1,7 @@
+# Beacon Distance Calculator
+# Copyright 2014 Geono Kim
+# Licensed under the Apache License, Version 2.0 (the "License");
+
 from curvefitted import CurveFittedDistanceCalculator
 
 
